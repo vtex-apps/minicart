9c9abb3d0d9c9829c363185709d762093f58d8b0
Vitória Heliane <vitoriaheliane@gmail.com>
2018-04-26 17:01:08 -0300

    Remove mock

--- react/MiniCart.js
+++ react/MiniCart.js
@@ -56,39 +56,10 @@ class MiniCart extends Component {
     let content
     if (data.loading) {
       content = this.renderLoading()
-    } else if (data.orderForm.length) {
+    } else if (!data.orderForm.length) {
       content = this.renderWithoutProducts(labelMiniCartEmpty)
     } else {
-      const orderForm = {
-        items: [
-          {
-            id: '31',
-            imageUrl: 'https://raw.githubusercontent.com/vtex-apps/product-summary/feature/product-image/images/500x500-img-pro20.png',
-            name: 'Motorola celular Moto X4',
-            quantity: 1,
-            sellingPrice: 154000,
-            skuName: 'Moto X4',
-          },
-          {
-            id: '32',
-            imageUrl: 'https://raw.githubusercontent.com/vtex-apps/product-summary/feature/product-image/images/500x500-img-pro1.png',
-            name: 'Motorola celular Moto X4',
-            quantity: 1,
-            sellingPrice: 154000,
-            skuName: 'Moto X4',
-          },
-          {
-            id: '33',
-            imageUrl: 'https://raw.githubusercontent.com/vtex-apps/product-summary/feature/product-image/images/500x500-img-pro1.png',
-            name: 'Motorola celular Moto X4',
-            quantity: 1,
-            sellingPrice: 154000,
-            skuName: 'Moto X4',
-          },
-        ],
-        value: 1632000,
-      }
-      content = this.renderMiniCartWithItems(orderForm, labelButton)
+      content = this.renderMiniCartWithItems(data.orderForm, labelButton)
     }
     return content
   }
