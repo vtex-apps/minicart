da64c71ebe6efeb2df77ff670d0530d4fda0369c
Vitória Heliane <vitoriaheliane@gmail.com>
2018-05-08 13:48:32 -0300

    Add minicart item link

---
+++ react/MiniCartContent.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { graphql, compose } from 'react-apollo'
+import { injectIntl, intlShape } from 'react-intl'
+import orderFormQuery from './graphql/orderFormQuery.gql'
+import MiniCartItem from './MiniCartItem'
+import Button from '@vtex/styleguide/lib/Button'
+import { Price } from '@vtex/product-details'
+import Spinner from '@vtex/styleguide/lib/Spinner'
+import './global.css'
+
+/**
+ * Minicart content component
+ */
+class MiniCartContent extends Component {
+  static propTypes = {
+    /* Products in the cart */
+    data: PropTypes.shape({
+      loading: PropTypes.bool.isRequired,
+      /* Order form */
+      orderForm: PropTypes.shape({
+        /* Order form id */
+        orderFormId: PropTypes.string,
+        /* Total price of the order */
+        value: PropTypes.number,
+        /* Items in the mini cart */
+        items: PropTypes.arrayOf(PropTypes.shape({
+          id: PropTypes.string,
+          /* Item's name */
+          name: PropTypes.string,
+          /* Item's url details */
+          detailUrl: PropTypes.string,
+          /* Item's image url */
+          imageUrl: PropTypes.string,
+          /* Item's quantity */
+          quantity: PropTypes.number,
+          /* Item's selling price */
+          sellingPrice: PropTypes.number,
+          /* Item's list price */
+          listPrice: PropTypes.number,
+          /* Item's sku name */
+          skuName: PropTypes.string,
+        }))
+      })
+    }).isRequired,
+    /* Mutate to update the items */
+    mutate: PropTypes.func.isRequired,
+    /* Label to appear when the minicart is empty */
+    labelMiniCartEmpty: PropTypes.string,
+    /* Label to appear in the finish shopping button */
+    labelButton: PropTypes.string,
+    /* Internationalization */
+    intl: intlShape.isRequired,
+  }
+
+  handleClickButton = () => location.assign('/checkout/#/cart')
+
+  renderWithoutItems = (label) => (
+    <div className="vtex-minicart__item pa4 shadow-4 flex items-center justify-center">
+      <span className="f5">{label}</span>
+    </div>
+  )
+
+  renderMiniCartWithItems = (orderForm, label) => (
+    <div className="flex flex-column" >
+      <div className="vtex-minicart__arrow-up self-end mr3 pr1"></div>
+      <div className="shadow-3">
+        <div className="vtex-minicart__content pa4 overflow-auto">
+          {orderForm.items.map(item => (
+            <div className="flex flex-row" key={item.id}>
+              <MiniCartItem
+                {...item} />
+            </div>
+          ))}
+        </div>
+        <div className="fl pr4 pl4 pb4">
+          <Button primary onClick={this.handleClickButton}>{label}</Button>
+        </div>
+        <div className="fr mt2 mr4">
+          <Price
+            sellingPrice={orderForm.value}
+            listPrice={orderForm.value}
+            showLabels={false}
+            showListPrice={false} />
+        </div>
+      </div>
+    </div >
+  )
+
+  renderLoading = () => (
+    <div className="vtex-minicart__item shadow-3 pa4 flex items-center justify-center">
+      <Spinner />
+    </div>
+  )
+
+  render() {
+    const { data, labelMiniCartEmpty, labelButton, intl, showRemoveButton } = this.props
+    console.log(data)
+    let content
+    if (data.loading) {
+      content = this.renderLoading()
+    } else if (!data.orderForm || !data.orderForm.items.length) {
+      content = this.renderWithoutItems(labelMiniCartEmpty || intl.formatMessage({ id: 'minicart-empty' }))
+    } else {
+      content = this.renderMiniCartWithItems(data.orderForm,
+        labelButton || intl.formatMessage({ id: 'finish-shopping-button-label' }))
+    }
+    return content
+  }
+}
+
+export default injectIntl(graphql(orderFormQuery)(MiniCartContent))
--- react/MiniCartItem.js
+++ react/MiniCartItem.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Price } from '@vtex/product-details'
+import CloseIcon from '@vtex/styleguide/lib/icon/Close'
+import Button from '@vtex/styleguide/lib/Button'
+import { Link } from 'render'
 import './global.css'
 
 /**
@@ -8,6 +11,8 @@ import './global.css'
  */
 export default class MiniCartItem extends Component {
   static propTypes = {
+    /* Item s id */
+    id: PropTypes.string.isRequired,
     /* Item's image */
     imageUrl: PropTypes.string.isRequired,
     /* Item's name */
@@ -18,31 +23,54 @@ export default class MiniCartItem extends Component {
     listPrice: PropTypes.number.isRequired,
     /* Item's sku */
     skuName: PropTypes.string.isRequired,
+    /* Detail url */
+    detailUrl: PropTypes.string.isRequired,
+  }
+
+  getItemId = detailUrl => {
+    const regExp = /\/([^)]+)\//
+    return regExp.exec(detailUrl)[1]
   }
 
   render() {
-    const { imageUrl, name, skuName, sellingPrice, listPrice } = this.props
+    const {
+      id,
+      detailUrl,
+      imageUrl,
+      name,
+      skuName,
+      sellingPrice,
+      listPrice
+    } = this.props
+
+    itemId = this.getItemId(detailUrl)
+
     return (
-      <div className="vtex-minicart__item flex flex-row relative bb b--silver mb3">
-        <img className="vtex-minicart__item-image" src={imageUrl} />
-        <div className="ml3">
-          <div className="vtex-minicart__item-name mt3">
-            <span className="b">{name}</span>
-          </div>
-          <div>
-            <div className="vtex-minicart__sku-name">
-              <span className="f7 dark-gray">{skuName}</span>
+      <Link
+        className="pointer link black-90"
+        page={'store/product'}
+        params={{ id: itemId }}>
+        <div className="vtex-minicart__item flex flex-row relative bb b--silver">
+          <img className="vtex-minicart__item-image" src={imageUrl} alt={name} />
+          <div className="ml3">
+            <div className="vtex-minicart__item-name mt3 tl">
+              <span className="b">{name}</span>
             </div>
-            <div className="absolute right-0 bottom-0 mb3">
-              <Price
-                sellingPrice={sellingPrice}
-                listPrice={listPrice}
-                showLabels={false}
-                showListPrice={false} />
+            <div>
+              <div className="vtex-minicart__sku-name tl">
+                <span className="f7 dark-gray">{skuName}</span>
+              </div>
+              <div className="absolute right-0 bottom-0 mb3 f3">
+                <Price
+                  sellingPrice={sellingPrice}
+                  listPrice={listPrice}
+                  showLabels={false}
+                  showListPrice={false} />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </Link>
     )
   }
 }
