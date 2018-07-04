93178c3781ffbc8f64a53bfb7adb0a388e6a2b9a
Vitória Heliane <vitoriaheliane@gmail.com>
2018-07-04 11:05:05 -0300

    Add the outside click handler into the sidebar

--- react/components/Sidebar.js
+++ react/components/Sidebar.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-import { IconCaretLeft } from 'vtex.styleguide'
+import { IconCaretRight } from 'vtex.styleguide'
 import { injectIntl, intlShape } from 'react-intl'
+
+import OutsideClickHandler from 'react-outside-click-handler'
 
 import MiniCart from '../MiniCart'
 
@@ -10,25 +12,32 @@ import '../global.css'
 
 const whiteColor = '#FFFFFF'
 
+/* SideBar component */
 class Sidebar extends Component {
   render() {
+    const { onBackClick, intl } = this.props
+
     if (typeof window !== 'undefined') {
       return ReactDOM.createPortal(
-        <div
-          className="vtex-minicart__sidebar fixed top-0 right-0 z-9999 bg-white shadow-2">
-          <div
-            className="vtex-minicart__sidebar-header pointer flex flex-row items-center pa5"
-            onClick={this.props.onBackClick}>
-            <IconCaretLeft size={18} color={whiteColor} />
-            <div className="mt3 ml4">
-              <MiniCart showContent miniCartIconColor={whiteColor} />
+        <OutsideClickHandler
+          onOutsideClick={onBackClick}
+        >
+          <div className="vtex-minicart__sidebar fixed top-0 right-0 z-9999 bg-white shadow-2">
+            <div
+              className="vtex-minicart__sidebar-header pointer flex flex-row items-center pa5"
+              onClick={onBackClick}
+            >
+              <IconCaretRight size={18} color={whiteColor} />
+              <div className="mt3 ml4">
+                <MiniCart showContent miniCartIconColor={whiteColor} />
+              </div>
+              <span className="ml4 white b ttu">{intl.formatMessage({ id: 'sidebar-title' })}</span>
             </div>
-            <span className="ml4 white b ttu">{this.props.intl.formatMessage({ id: 'sidebar-title' })}</span>
+            <div className="h-100">
+              {this.props.children}
+            </div>
           </div>
-          <div className="h-100">
-            {this.props.children}
-          </div>
-        </div>,
+        </OutsideClickHandler>,
         document.body
       )
     }
