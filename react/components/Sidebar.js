050c03004c9593761f1dc8b5205e10be4938c3ab
Vitória Heliane <vitoriaheliane@gmail.com>
2018-08-06 15:55:34 -0300

    Add animation into the sidebar

--- react/components/Sidebar.js
+++ react/components/Sidebar.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import { IconCaretRight } from 'vtex.styleguide'
 import { injectIntl, intlShape } from 'react-intl'
 import OutsideClickHandler from 'react-outside-click-handler'
+import { Transition } from 'react-spring'
 
 import MiniCart from '../MiniCart'
 
@@ -19,26 +20,42 @@ class Sidebar extends Component {
     document.body.classList.remove('vtex-minicart-sidebar-open')
   }
 
-  render() {
+  renderChildren = styles => {
     const { onOutsideClick, intl } = this.props
 
-    if (typeof window !== 'undefined') {
-      return ReactDOM.createPortal(
-        <OutsideClickHandler onOutsideClick={onOutsideClick}>
-          <div className="vtex-minicart__sidebar fixed top-0 right-0 z-9999 bg-white shadow-2 flex flex-column">
-            <div
-              className="vtex-minicart__sidebar-header pointer flex flex-row items-center pa5"
-              onClick={onOutsideClick}
-            >
-              <IconCaretRight size={18} color={WHITE_COLOR} />
-              <div className="mt3 ml4">
-                <MiniCart hideContent miniCartIconColor={WHITE_COLOR} />
-              </div>
-              <span className="ml4 white b ttu">{intl.formatMessage({ id: 'sidebar-title' })}</span>
+    return (
+      <OutsideClickHandler onOutsideClick={onOutsideClick}>
+        <div className="vtex-minicart__sidebar fixed top-0 right-0 z-9999 bg-white shadow-2 flex flex-column"
+          style={styles}>
+          <div
+            className="vtex-minicart__sidebar-header pointer flex flex-row items-center pa5"
+            onClick={onOutsideClick}
+          >
+            <IconCaretRight size={18} color={WHITE_COLOR} />
+            <div className="mt3 ml4">
+              <MiniCart hideContent miniCartIconColor={WHITE_COLOR} />
             </div>
-            {this.props.children}
+            <span className="ml4 white b ttu">{intl.formatMessage({ id: 'sidebar-title' })}</span>
           </div>
-        </OutsideClickHandler>,
+          {this.props.children}
+        </div>
+      </OutsideClickHandler>
+    )
+  }
+
+  render() {
+    const { isOpen } = this.props
+
+    if (typeof document !== 'undefined') {
+      return ReactDOM.createPortal(
+        <Transition
+          keys={isOpen ? ['children'] : []}
+          from={{ transform: 'translateX(100%)' }}
+          enter={{ transform: 'translateX(0%)' }}
+          leave={{ transform: 'translateX(100%)' }}
+        >
+          {isOpen ? [this.renderChildren] : []}
+        </Transition>,
         document.body
       )
     }
@@ -49,6 +66,8 @@ class Sidebar extends Component {
 Sidebar.propTypes = {
   /* Internationalization */
   intl: intlShape.isRequired,
+  /* Set the sideBar visibility */
+  isOpen: PropTypes.bool,
   /* Sidebar content */
   children: PropTypes.object.isRequired,
   /* Function to be called when click in the close sidebar button or outside the sidebar */
