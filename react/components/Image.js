825d634b453362b7cd2c9b6195fe69e6db94ed8d
Vitória Heliane <vitoriaheliane@gmail.com>
2018-08-08 15:50:38 -0300

    Add image loader

---
+++ react/components/Image.js
@@ -0,0 +1,40 @@
+import React, { Component, Fragment } from 'react'
+import PropTypes from 'prop-types'
+import ContentLoader from 'react-content-loader'
+
+export default class Image extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLaoded: false,
+    }
+  }
+
+  stripImageUrl(url) {
+    return url.replace(/^(http(s?):)/, '')
+  }
+
+  imgLoader = () => (
+    <ContentLoader height={140} width={140} speed={1}>
+      <rect x="0" y="0" rx="5" ry="5" width="500" height="500" />
+    </ContentLoader>
+  )
+
+  render() {
+    const { imageUrl, alt } = this.props
+
+    return (
+      <Fragment>
+        <img className="vtex-minicart__item-image" onLoad={() => this.setState({ isLaoded: true })} src={this.stripImageUrl(imageUrl)} alt={alt} />
+        {!this.state.isLaoded && this.imgLoader()}
+      </Fragment>
+    )
+  }
+}
+
+Image.propTypes = {
+  /* Image's url */
+  imageUrl: PropTypes.string.isRequired,
+  /* Image's alt */
+  alt: PropTypes.string.isRequired,
+}
