8ba0046150c016fbc19d06f12623facbd4f51129
Vitória Heliane <vitoriaheliane@gmail.com>
2018-09-12 14:23:01 -0300

    Refactor the Popup Component CSS

--- react/components/Popup.js
+++ react/components/Popup.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import OutsideClickHandler from 'react-outside-click-handler'
-import classNames from 'classnames'
 
 /**
  * Pop-up component.
@@ -9,15 +8,10 @@ import classNames from 'classnames'
 export default class Popup extends Component {
   render() {
     const {
-      showDiscount,
       children,
       onOutsideClick,
       buttonOffsetWidth,
     } = this.props
-    const classes = classNames('mt3 bg-white relative', {
-      'vtex-minicart__content-container--footer-small': !showDiscount,
-      'vtex-minicart__content-container--footer-large': showDiscount,
-    })
 
     const boxPositionStyle = {
       right: buttonOffsetWidth && buttonOffsetWidth - 49,
@@ -26,12 +20,12 @@ export default class Popup extends Component {
     return (
       <OutsideClickHandler onOutsideClick={onOutsideClick}>
         <div
-          className="vtex-minicart__box absolute z-max flex flex-colunm"
+          className="vtex-minicart__box dn db-ns absolute z-max flex flex-colunm"
           style={boxPositionStyle}
         >
           <div className="shadow-3">
-            <div className="vtex-minicart__arrow-up absolute top-0 right-0 shadow-3" />
-            <div className={classes}>{children}</div>
+            <div className="vtex-minicart__arrow-up absolute top-0 shadow-3 bg-white h2 w2 pa2 rotate-45" />
+            <div className="mt3 bg-white relative flex flex-column">{children}</div>
           </div>
         </div>
       </OutsideClickHandler>
@@ -41,7 +35,6 @@ export default class Popup extends Component {
 
 Popup.propTypes = {
   children: PropTypes.object,
-  showDiscount: PropTypes.bool,
   buttonOffsetWidth: PropTypes.number,
   /* Function to be called when click occurs outside the popup */
   onOutsideClick: PropTypes.func,
