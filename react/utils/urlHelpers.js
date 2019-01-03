3cac88ee2ffe7aa75610f3403746a7aca2bb8b74
Gustavo Rosolem <guspace92@hotmail.com>
2019-01-03 18:59:34 -0200

    Replace image URL just for legacy file manager

--- react/utils/urlHelpers.js
+++ react/utils/urlHelpers.js
@@ -4,13 +4,17 @@ export const MAX_WIDTH = 3000
 export const MAX_HEIGHT = 4000
 
 /**
- * Having the url below as base,
+ * Having the url below as base for the LEGACY file manager,
  * https://storecomponents.vteximg.com.br/arquivos/ids/155472/Frame-3.jpg?v=636793763985400000
  * the following regex will match https://storecomponents.vteximg.com.br/arquivos/ids/155472
  *
  * Also matches urls with defined sizes like:
  * https://storecomponents.vteximg.com.br/arquivos/ids/155473-160-auto
  * @type {RegExp}
+ *
+ * On the new vtex.file-manager isn't necessary replace the URL, just add the param on the querystring, like:
+ * "?width=WIDTH&height=HEIGHT&aspect=true"
+ *
  */
 const baseUrlRegex = new RegExp(/.+ids\/(\d+)/)
 
@@ -25,6 +29,13 @@ export function cleanImageUrl(imageUrl) {
   if (result.length > 0) return result[0]
 }
 
+function replaceLegacyFileManager(imageUrl, width, height) {
+  if (!imageUrl.includes('/arquivos/ids/')) return imageUrl
+  imageUrl = cleanImageUrl(imageUrl)
+  imageUrl = `${imageUrl}-${width}-${height}`
+  return imageUrl
+}
+
 export function changeImageUrlSize(
   imageUrl,
   width = DEFAULT_WIDTH,
@@ -34,7 +45,8 @@ export function changeImageUrlSize(
   typeof width === 'number' && (width = Math.min(width, MAX_WIDTH))
   typeof height === 'number' && (height = Math.min(height, MAX_HEIGHT))
 
-  imageUrl = cleanImageUrl(imageUrl)
+  imageUrl = replaceLegacyFileManager(imageUrl, width, height)
+  imageUrl = `${imageUrl}${imageUrl.split('?')[1] ? '&' : '?'}width=${width}&height=${height}&aspect=true`
 
-  return `${imageUrl}-${width}-${height}`
+  return imageUrl
 }
