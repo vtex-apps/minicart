4c8f707686674f338a63a1d0ecd602d7c0ff50d1
Estácio Pereira <estacio.pereira@gmail.com>
2019-02-15 11:04:23 -0300

    Update orderForm proptypes

--- react/propTypes.js
+++ react/propTypes.js
@@ -25,6 +25,36 @@ export const MiniCartPropTypes = {
   showSku: PropTypes.bool,
   /* Set the visibility of the Quantity Selector */
   enableQuantitySelector: PropTypes.bool,
+  /* The orderForm object */
+  orderForm: PropTypes.shape({
+    /* Order form id */
+    orderFormId: PropTypes.string,
+    /* Total price of the order */
+    value: PropTypes.number,
+    /* Items in the mini cart */
+    items: PropTypes.arrayOf(PropTypes.object),
+    /* Shipping Address */
+    shippingData: PropTypes.shape({
+      address: PropTypes.shape({
+        addressName: PropTypes.string,
+        addressType: PropTypes.string,
+        city: PropTypes.string,
+        complement: PropTypes.string,
+        country: PropTypes.string,
+        id: PropTypes.string,
+        neighborhood: PropTypes.string,
+        number: PropTypes.string,
+        postalCode: PropTypes.string,
+        receiverName: PropTypes.string,
+        reference: PropTypes.string,
+        state: PropTypes.string,
+        street: PropTypes.string,
+        userId: PropTypes.string,
+      }),
+      /* Available Addresses */
+      availableAddresses: PropTypes.arrayOf(PropTypes.object),
+    }),
+  }).isRequired,
   /* Max quantity for the Quantity Selector */
   maxQuantity: PropTypes.number,
   /* Set the shipping fee visibility */
