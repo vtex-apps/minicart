4d817e6aa4bdde5f51ce61c3e87bdfe2b72b8e66
Estácio Pereira <estacio.pereira@gmail.com>
2019-02-15 09:46:03 -0300

    Remove log

--- react/index.js
+++ react/index.js
@@ -60,7 +60,6 @@ export class MiniCart extends Component {
 
   handleItemsUpdate = async () => {
     const clientOnlyItems = this.getClientOnlyItems()
-    console.log(clientOnlyItems)
     if (clientOnlyItems.length && !this.state.updatingOrderForm) {
       return this.handleItemsDifference(clientOnlyItems)
     }
