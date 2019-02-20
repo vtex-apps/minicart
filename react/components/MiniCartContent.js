7b7c24cd8f58237e67b975aad684211bd871dbba
matheusps <matheus.ps@icloud.com>
2019-02-20 15:09:50 -0300

    Fix total discount not showing

--- react/components/MiniCartContent.js
+++ react/components/MiniCartContent.js
@@ -14,6 +14,7 @@ import {
   groupItemsWithParents,
   getOptionChoiceType,
   getOptionComposition,
+  getTotalDiscount,
 } from '../utils/itemsHelper'
 
 import minicart from '../minicart.css'
@@ -278,6 +279,7 @@ class MiniCartContent extends Component {
           shippingCost={this.getShippingCost(orderForm)}
           isUpdating={this.isUpdating}
           totalValue={orderForm.value}
+          discount={this.calculateDiscount(orderForm.items, orderForm.value)}
           buttonLabel={label}
           isSizeLarge={isSizeLarge}
           labelDiscount={labelDiscount}
