895a7fad5630717310d5f9f7928c5b1fa5c480a4
Juan Almeida <juan.satyro@gmail.com>
2019-03-13 18:09:20 -0300

    Add behavior test in minicart

--- react/__tests__/MiniCart.test.js
+++ react/__tests__/MiniCart.test.js
@@ -8,6 +8,54 @@ describe('<MiniCart /> component', () => {
     expect(asFragment()).toBeDefined()
   })
 
+  it('should render popup onClick', () => {
+    const { getByText, baseElement } = render(
+      <MiniCart type="popup" hideContent={false} />
+    )
+
+    let box = baseElement.querySelector('.box')
+    let sidebar = baseElement.querySelector('.sidebar')
+
+    // Expect box be null before click
+    expect(box).toBeNull()
+    // Sidebar should not exist before and after click
+    expect(sidebar).toBeNull()
+
+    fireEvent.click(getByText('Button Test'))
+
+    box = baseElement.querySelector('.box')
+    sidebar = baseElement.querySelector('.sidebar')
+
+    // Expect Minicart Popup redered
+    expect(box).toBeDefined()
+    expect(box).not.toBeNull()
+    // Expect sidebar still not rendered
+    expect(sidebar).toBeNull()
+  })
+
+  it('should render sidebar onClick', () => {
+    const { getByText, baseElement } = render(
+      <MiniCart type="sidebar" hideContent={false} />
+    )
+
+    let box = baseElement.querySelector('.box')
+    let sidebar = baseElement.querySelector('.sidebarScrim.dn')
+
+    // Expect box be null before and after click
+    expect(box).toBeNull()
+    // Sidebar should shold have diplay block class before click
+    expect(sidebar).toBeDefined()
+    expect(sidebar).not.toBeNull()
+
+    fireEvent.click(getByText('Button Test'))
+
+    box = baseElement.querySelector('.box')
+    sidebar = baseElement.querySelector('.sidebarScrim.dn')
+
+    expect(sidebar).toBeNull()
+    expect(box).toBeNull()
+  })
+
   it('should match the snapshot in popup mode', () => {
     const leftClick = { button: 0 }
 
