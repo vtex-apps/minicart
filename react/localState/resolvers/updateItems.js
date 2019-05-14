412bc3f732bcef7b67c0d1416952701465d39f84
Joao Fidelis <joaogfidelis@gmail.com>
2019-05-14 11:24:36 -0300

    update cartIndex locally when deleting items, so next requests have correct index and dont fail

--- react/localState/resolvers/updateItems.js
+++ react/localState/resolvers/updateItems.js
@@ -2,10 +2,17 @@ import { mergeDeepRight } from 'ramda'
 
 import { mapToMinicartItem, ITEMS_STATUS } from '../index'
 
+const shouldBeInCart = (item, itemsRemovedIndexes) =>
+  item.quantity !== 0 && !itemsRemovedIndexes.includes(item.parentItemIndex)
+
 const updateItems = (cartItems, newItems) => {
   // Items provided to this function MUST have a valid index property
   const cleanNewItems = newItems.filter(({ index }) => index != null)
   const items = [...cartItems]
+
+  const itemsToBeRemoved = cleanNewItems
+    .filter(({ quantity }) => quantity === 0)
+    .map(({ cartIndex }) => cartIndex)
 
   for (const newItem of cleanNewItems) {
     const { index } = newItem
@@ -17,6 +24,20 @@ const updateItems = (cartItems, newItems) => {
       })
     )
   }
+
+  let itemsRemoved = 0
+  if (itemsToBeRemoved.length > 0) {
+    for (const item of items) {
+      if (shouldBeInCart(item, itemsToBeRemoved)) {
+        item.cartIndex -= itemsRemoved
+      } else {
+        itemsRemoved += 1
+        // Set quantity to 0 for children of parent assembly options to be removed
+        item.quantity = 0
+      }
+    }
+  }
+
   return items.filter(Boolean)
 }
 
