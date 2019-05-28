b0a0b24df38483f832cf03b13a06eac9c46999c9
mariana-mendes <mariana.silva@ccc.ufcg.edu.br>
2019-05-28 15:50:07 -0300

    Remove IOMessage

--- react/__mocks__/vtex.native-types/index.js
+++ react/__mocks__/vtex.native-types/index.js
@@ -1,8 +1,1 @@
-
-import React from 'react'
-import { FormattedMessage } from 'react-intl'
-
 export const formatIOMessage = ({ id }) => id
-
-export const IOMessage = props =>
-props.id ? <FormattedMessage {...props} /> : null
