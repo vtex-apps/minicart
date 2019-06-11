3e4495d34c8b32897e905e03c80638b17301be51
Thayanne Sousa <thayannevls@gmail.com>
2019-06-11 18:31:47 -0300

    check if minicartitems linkstate is empty

--- react/index.js
+++ react/index.js
@@ -1,7 +1,7 @@
 import classNames from 'classnames'
 import hoistNonReactStatics from 'hoist-non-react-statics'
 import PropTypes from 'prop-types'
-import { map, partition, path, pathOr, pick } from 'ramda'
+import { map, partition, path, pathOr, pick, isEmpty } from 'ramda'
 import React, { Component, useEffect } from 'react'
 import { Button, withToast } from 'vtex.styleguide'
 import { isMobile } from 'react-device-detect'
@@ -108,7 +108,8 @@ class MiniCart extends Component {
         await this.props.updateOrderForm(orderForm)
         localStorage.removeItem('orderForm')
       }
-      if (minicart && minicart.length) {
+      if (minicart && minicart.length && 
+        isEmpty(pathOr([], ['linkState', 'minicartItems'], this.props))) {
         await this.props.addToLinkStateCart(minicart)
         localStorage.removeItem('minicart')
       }
