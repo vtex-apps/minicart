6e50381c602c7f7f0b1d4b5151688c13cdef6ef5
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-07-02 14:40:39 -0300

    fix tests

--- react/__mocks__/vtex.pixel-manager/PixelContext.js
+++ react/__mocks__/vtex.pixel-manager/PixelContext.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 const push = () => null
 
-export const withPixel = WrappedComponent => props => (
-  <WrappedComponent {...props} push={push} />
-)
+export const withPixel = WrappedComponent => {
+  const withPixel = props => <WrappedComponent {...props} push={push} />
+  return withPixel
+}
+
+export const usePixel = () => ({
+  push: jest.fn(),
+})
