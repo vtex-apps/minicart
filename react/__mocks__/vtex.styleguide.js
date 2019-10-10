c92ea60c16dbe072593d055a80df11ec8b85693f
Lucas Bebber <lucasbbebber@gmail.com>
2019-10-10 14:54:48 -0300

    Update tests

--- react/__mocks__/vtex.styleguide.js
+++ react/__mocks__/vtex.styleguide.js
@@ -4,6 +4,10 @@ export const Button = ({ icon, ...props }) => (
   <button {...props}>Button test{props.children}</button>
 )
 
+export const ButtonWithIcon = ({ icon, ...props }) => (
+  <button {...props}>Button test{icon} {props.children}</button>
+)
+
 export const Spinner = ({ children }) => <div>{children}</div>
 
 export const ToastContext = React.createContext({
