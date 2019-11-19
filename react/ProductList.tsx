bdcc1a2d5aff6f084de2c9da0952b3521cb58ec1
victorhmp <victor2142@gmail.com>
2019-11-19 16:28:34 -0300

    Implement pixel events
    Add support for sending `'addToCart'` events on quantity change.
    
    Add support for sending `'removeFromCart'` events on item removal.

--- react/ProductList.tsx
+++ react/ProductList.tsx
@@ -2,16 +2,57 @@ import React, { FC } from 'react'
 import { useOrderForm } from 'vtex.order-manager/OrderForm'
 import { OrderItemsProvider, useOrderItems } from 'vtex.order-items/OrderItems'
 import { ExtensionPoint } from 'vtex.render-runtime'
+import { usePixel } from 'vtex.pixel-manager/PixelContext'
+
+const adjustSkuItemForPixelEvent = (
+  skuItem: OrderFormItem,
+  quantity?: number
+) => {
+  const categoryIds = skuItem.productCategoryIds.split('/')
+  const itemCategory = categoryIds
+    .map(categoryId => skuItem.productCategories[categoryId])
+    .join('/')
+    .slice(1, -1)
+
+  return {
+    skuId: skuItem.id,
+    variant: skuItem.skuName,
+    price: skuItem.price,
+    name: skuItem.name,
+    quantity: quantity || skuItem.quantity,
+    productRefId: skuItem.productRefId,
+    brand: skuItem.additionalInfo.brandName,
+    category: itemCategory,
+  }
+}
 
 const ProductList: FC = () => {
   const {
     orderForm: { items },
   } = useOrderForm()
   const { updateQuantity, removeItem } = useOrderItems()
+  const { push } = usePixel()
 
-  const handleQuantityChange = (uniqueId: string, quantity: number) =>
+  const handleQuantityChange = (
+    uniqueId: string,
+    quantity: number,
+    item: OrderFormItem
+  ) => {
+    const adjustedItem = adjustSkuItemForPixelEvent(item, quantity)
+    push({
+      event: 'addToCart',
+      items: [adjustedItem],
+    })
     updateQuantity({ uniqueId, quantity })
-  const handleRemove = (uniqueId: string) => removeItem({ uniqueId })
+  }
+  const handleRemove = (uniqueId: string, item: OrderFormItem) => {
+    const adjustedItem = adjustSkuItemForPixelEvent(item)
+    push({
+      event: 'removeFromCart',
+      items: [adjustedItem],
+    })
+    removeItem({ uniqueId })
+  }
 
   return (
     <div className="overflow-y-scroll">
