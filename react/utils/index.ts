c0db0a3765299896233f154b0491e406df1cec08
victorhmp <victor2142@gmail.com>
2019-11-19 16:26:39 -0300

    Delete unnecessary utils files

--- react/utils/index.ts
+++
@@ -1,5 +0,0 @@
-const isParentItem = ({ parentItemIndex, parentAssemblyBinding }: any) =>
-  parentItemIndex == null && parentAssemblyBinding == null
-
-export const shouldShowItem = (item: OrderFormItem) =>
-  !!item.quantity && isParentItem(item)
