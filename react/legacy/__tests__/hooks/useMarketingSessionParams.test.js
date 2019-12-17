fcb2e580b53392a0b8a143f419450acc47f20a8e
Joao Fidelis <joaogfidelis@gmail.com>
2019-12-17 16:03:20 -0300

    use wait and not act

--- react/legacy/__tests__/hooks/useMarketingSessionParams.test.js
+++ react/legacy/__tests__/hooks/useMarketingSessionParams.test.js
@@ -1,4 +1,4 @@
-import { renderHook, act } from '@vtex/test-tools/react'
+import { renderHook, wait } from '@vtex/test-tools/react'
 import useMarketingSessionParams from '../../hooks/useMarketingSessionParams'
 
 const fakeSessionWithData = {
@@ -41,7 +41,7 @@ it('if session has correct data, get and format input correctly', async () => {
   window.__RENDER_8_SESSION__.sessionPromise = new Promise(resolve => resolve(fakeSessionWithData))
   const { result } = renderHook(() => useMarketingSessionParams())
   
-  await act(async () => await Promise.resolve())
+  await wait()
 
   const { utmParams, utmiParams } = result.current
   expect(utmParams).toMatchObject({
@@ -61,7 +61,7 @@ it('if session does not have data, leave object as undefined', async () => {
   window.__RENDER_8_SESSION__.sessionPromise = new Promise(resolve => resolve(fakeSessionNoData))
   const { result } = renderHook(() => useMarketingSessionParams())
 
-  await act(async () => await Promise.resolve())
+  await wait()
 
   const { utmParams, utmiParams } = result.current
   expect(utmParams).toBe(undefined)
@@ -73,7 +73,7 @@ it('if session promise rejects, everything still works, response is undefined', 
   window.__RENDER_8_SESSION__.sessionPromise = new Promise((_, reject) => reject())
   const { result } = renderHook(() => useMarketingSessionParams())
 
-  await act(async () => await Promise.resolve())
+  await wait()
 
   const { utmParams, utmiParams } = result.current
   expect(utmParams).toBe(undefined)
