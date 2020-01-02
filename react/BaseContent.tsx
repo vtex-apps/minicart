bbf37a0dda3b1f7c99714da8d1799bbe50b277a2
Jefferson <jefiim.ad@gmail.com>
2020-01-02 19:08:58 -0300

    Not use the navigate function if it's proceeding to an url different than the checkout cart one

--- react/BaseContent.tsx
+++ react/BaseContent.tsx
@@ -47,7 +47,7 @@ const Content: FC<Props> = ({ finishShoppingButtonLink, children }) => {
   const { navigate } = useRuntime()
 
   const goToCheckout = (url: string) => {
-    if (major) {
+    if (major && url === checkoutUrl) {
       navigate({ to: url })
     } else {
       window.location.href = url
