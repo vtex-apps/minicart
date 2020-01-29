d592e5cd4d61313a10146c9386ef9061aa679c9d
victorhmp <victor2142@gmail.com>
2020-01-29 15:32:02 -0300

    Bring back click anywhere to close popup

--- react/components/Popup.tsx
+++ react/components/Popup.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { FC, Fragment } from 'react'
+import React, { FC } from 'react'
 import { Overlay } from 'vtex.react-portal'
 import { useCssHandles } from 'vtex.css-handles'
 
@@ -25,44 +25,49 @@ const PopupMode: FC = ({ children }) => {
   const dispatch = useMinicartDispatch()
   const handles = useCssHandles(CSS_HANDLES)
 
+  const clickHandler = () => {
+    if (openOnHoverProp) {
+      dispatch({ type: 'SET_OPEN_ON_HOVER_BEHAVIOR', value: true })
+    }
+    dispatch({ type: 'CLOSE_MINICART' })
+  }
+  const mouseLeaveHandler = () => {
+    if (!openOnHoverBehavior) {
+      return
+    }
+    dispatch({ type: 'CLOSE_MINICART' })
+  }
+
   return (
-    <Fragment>
-      <div
-        onMouseLeave={
-          openOnHoverBehavior
-            ? () => dispatch({ type: 'CLOSE_MINICART' })
-            : undefined
-        }
-      >
-        <MinicartIconButton />
-        {open && (
-          <Overlay>
-            {!openOnHoverProp && (
-              <div
-                className="fixed top-0 left-0 w-100 h-100"
-                onClick={() => dispatch({ type: 'CLOSE_MINICART' })}
-              />
-            )}
+    <div onMouseLeave={mouseLeaveHandler}>
+      <MinicartIconButton />
+      {open && (
+        <Overlay>
+          {!openOnHoverBehavior && (
             <div
-              className={`${handles.popupWrapper} ${styles.popupBoxPosition} absolute z-max flex flex-colunm`}
+              className="fixed top-0 left-0 w-100 h-100"
+              onClick={clickHandler}
+            />
+          )}
+          <div
+            className={`${handles.popupWrapper} ${styles.popupBoxPosition} absolute z-max flex flex-colunm`}
+          >
+            <div
+              className={`${handles.popupContentContainer} w-100 shadow-3 bg-base`}
             >
               <div
-                className={`${handles.popupContentContainer} w-100 shadow-3 bg-base`}
+                className={`${handles.arrowUp} ${styles.popupArrowUp} absolute top-0 bg-base h1 w1 pa4 rotate-45`}
+              />
+              <div
+                className={`${handles.popupChildrenContainer} mt3 bg-base relative flex flex-column ph5 pv3`}
               >
-                <div
-                  className={`${handles.arrowUp} ${styles.popupArrowUp} absolute top-0 bg-base h1 w1 pa4 rotate-45`}
-                />
-                <div
-                  className={`${handles.popupChildrenContainer} mt3 bg-base relative flex flex-column ph5 pv3`}
-                >
-                  {hasBeenOpened && children}
-                </div>
+                {hasBeenOpened && children}
               </div>
             </div>
-          </Overlay>
-        )}
-      </div>
-    </Fragment>
+          </div>
+        </Overlay>
+      )}
+    </div>
   )
 }
 
