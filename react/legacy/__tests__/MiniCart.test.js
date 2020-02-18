02653e9e379e76e8334582651cc935a11c6a937e
Joao Fidelis <joaogfidelis@gmail.com>
2020-02-18 10:18:18 -0300

    erase comment

--- react/legacy/__tests__/MiniCart.test.js
+++ react/legacy/__tests__/MiniCart.test.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { render, fireEvent, wait } from '@vtex/test-tools/react'
-// import { orderForm as orderFormQuery } from 'vtex.store-resources/Queries'
 import orderFormQuery from 'vtex.store-resources/QueryOrderForm'
 
 import orderForm from '../__mocks__/orderForm'
