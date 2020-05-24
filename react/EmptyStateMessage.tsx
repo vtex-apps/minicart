2700bb000facbf86ec403cf21cd2c1b3365c4ce8
victorhmp <victor2142@gmail.com>
2020-05-24 16:13:39 -0300

    Add CSS handles to EmptyStateMessage component

--- react/EmptyStateMessage.tsx
+++ react/EmptyStateMessage.tsx
@@ -1,19 +1,22 @@
 import React, { FC, Fragment } from 'react'
 import { FormattedMessage } from 'react-intl'
+import { useCssHandles } from 'vtex.css-handles'
 
 import EmptyStateIcon from './components/EmptyStateIcon'
 
+const CSS_HANDLES = ['emptyStateIconWrapper', 'emptyStateMessage'] as const
+
 const EmptyStateMessage: FC = () => {
+  const handles = useCssHandles(CSS_HANDLES)
+
   return (
     <Fragment>
-      <div className="tc pv8">
+      <div className={`${handles.emptyStateIconWrapper} tc pv8`}>
         <EmptyStateIcon />
       </div>
-      <div className="c-muted-2 pv9 tc">
-        <p>
-          <FormattedMessage id="store/minicart.empty-state" />
-        </p>
-      </div>
+      <p className={`${handles.emptyStateMessage} c-muted-1 pv9 tc`}>
+        <FormattedMessage id="store/minicart.empty-state" />
+      </p>
     </Fragment>
   )
 }
