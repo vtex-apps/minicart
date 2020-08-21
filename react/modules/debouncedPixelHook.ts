a2a5c20add917d46889248d6f47f3c631e085336
victorhmp <victor2142@gmail.com>
2020-08-21 14:17:14 -0300

    Update type definition for 'debouncedPush' variable

--- react/modules/debouncedPixelHook.ts
+++ react/modules/debouncedPixelHook.ts
@@ -2,11 +2,11 @@ import { useRef } from 'react'
 import { usePixel } from 'vtex.pixel-manager/PixelContext'
 import debounce from 'debounce'
 
+type DebounceFunction = (param: Record<string, unknown>) => void
+
 export default function useDebouncedPush() {
   const { push } = usePixel()
-  const debouncedPush = useRef<(param: Record<string, unknown>) => void>(
-    debounce(push, 600)
-  )
+  const debouncedPush = useRef<DebounceFunction>(debounce(push, 600))
 
   return debouncedPush.current
 }
