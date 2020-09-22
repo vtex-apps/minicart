65764eb50a4573c89ddc247ac2c50b0972c84879
Vitor Gomes <vitorlgomes@hotmail.com>
2020-09-22 10:40:24 -0300

    Use navigate when redirecting to checkout

--- react/modules/checkoutHook.ts
+++ react/modules/checkoutHook.ts
@@ -3,13 +3,13 @@ import { useRuntime } from 'vtex.render-runtime'
 
 export default function useCheckout() {
   const { url: checkoutUrl, major } = useCheckoutURL()
-  const { navigate, rootPath = '' } = useRuntime()
+  const { navigate  } = useRuntime()
 
   const goToCheckout = (url: string) => {
     if (major > 0 && url === checkoutUrl) {
       navigate({ to: url })
     } else {
-      window.location.href = `${rootPath}${url}`
+      navigate({ to: url, fallbackToWindowLocation: true })
     }
   }
 
