765913a10590626ac8aa72a502e920995a47018a
Alexandre Siqueira <alexandre.siqueira@trinto.com.br>
2021-10-05 16:41:49 -0300

    Adobe launch implementation, click gotToCheckout button

--- react/CheckoutButton.tsx
+++ react/CheckoutButton.tsx
@@ -3,9 +3,10 @@ import { FormattedMessage } from 'react-intl'
 import { Button } from 'vtex.styleguide'
 import { CssHandlesTypes, useCssHandles } from 'vtex.css-handles'
 import { useCheckoutURL } from 'vtex.checkout-resources/Utils'
-
+import { useOrderForm } from 'vtex.order-manager/OrderForm'
+import { mapCartItemToPixel } from './modules/pixelHelper'
+import { usePixel } from 'vtex.pixel-manager'
 import useCheckout from './modules/checkoutHook'
-
 const CSS_HANDLES = ['minicartCheckoutButton'] as const
 
 interface Props {
@@ -16,13 +17,31 @@ interface Props {
 const CheckoutButton: FC<Props> = ({ finishShoppingButtonLink, classes }) => {
   const { url: checkoutUrl } = useCheckoutURL()
   const { handles } = useCssHandles(CSS_HANDLES, { classes })
+  const { orderForm }: OrderFormContext = useOrderForm()
+  const { push } = usePixel()
+
   const goToCheckout = useCheckout()
+
+  const emitEventAdobeLaunch = () => {   
+    if(orderForm.items?.length > 0){
+      const data = orderForm.items.map(mapCartItemToPixel)
+      push({
+        event: "cart",
+        items: data
+      })
+    }
+  }
+
+  const handleClick = () => {
+    emitEventAdobeLaunch()
+    goToCheckout(finishShoppingButtonLink || checkoutUrl)
+  }
 
   return (
     <div className={`${handles.minicartCheckoutButton} mv3 ph4 ph6-l`}>
       <Button
         id="proceed-to-checkout"
-        onClick={() => goToCheckout(finishShoppingButtonLink || checkoutUrl)}
+        onClick={handleClick}
         variation="primary"
         block
       >
